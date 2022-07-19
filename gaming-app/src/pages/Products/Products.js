04fd209abff3897ad3bc5109574a4e872be95190
Xenia Radulovic <ksenija.radulovic@outlook.com>
2022-07-19 09:17:55 +0200

    creates product page

---
+++ gaming-app/src/pages/Products/Products.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const Products = () => {
+    return (
+        <div>
+            PRODUCTSSSS
+        </div>
+    )
+}
+
+
+export default Products;
